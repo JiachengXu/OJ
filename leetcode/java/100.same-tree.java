c0e4cacbdb6c70f8d382e5dbee85f08a604f88f3
Jiacheng Xu <xjcmaxwellcjx@gmail.com>
2021-12-01 17:49:34 +0000

    Update leetcode solutions by leetdoad.

--- leetcode/java/100.same-tree.java
+++ leetcode/java/100.same-tree.java
@@ -9,10 +9,10 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p==null&&q==null) return true;
-        if(p==null) return false;
-        if(q==null) return false;
-        if(p.val==q.val) return isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
+        if(p == null && q == null) return true;
+        if(p == null) return false;
+        if(q == null) return false;
+        if(p.val == q.val) return isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
         return false;
     }
 }
